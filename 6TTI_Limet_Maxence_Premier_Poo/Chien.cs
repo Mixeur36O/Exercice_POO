515c7a273a67754e70cfc00eeab3337672bf7d45
Limet Maxence <200040@CCM-Asty-Moulin.local>
2025-09-29 09:42:56 +0200

    29/29/2025 ex 1

---
+++ 6TTI_Limet_Maxence_Premier_Poo/Chien.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_Limet_Maxence_Premier_Poo
+{
+    internal class Chien
+    {
+        //Attribut
+        private string _nom;
+        private int _age;
+        private string _race;
+
+        //Constructeur
+        public Chien(string nom, int age, string race)
+        {
+            _nom = nom;
+            _age = age;
+            _race = race;
+        }
+
+        //Méthode
+        public string Affichercaracteristique()
+        {
+            return $"Nom : {_nom} - Age : {_age} - Race : {_race}";
+        }
+
+        public void Aliment(out string aliments)
+        {
+            Console.WriteLine("Que voulez vous donner à manger à votre chien ?");
+            aliments = Console.ReadLine();
+        }
+        public string Manger(string aliments)
+        {
+            string info = $"{_nom} à manger {aliments} ";
+            return info;
+        }
+
+        public string Aboyer()
+        {
+            string info = "WOUAF WOUAF !";
+            return info;
+        }
+
+        public void veillir()
+        {
+            _age += 1;
+        }
+
+    }
+
+}
---
+++ 6TTI_Limet_Maxence_Premier_Poo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _6TTI_Limet_Maxence_Premier_Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chien[] mesChiens = new Chien[3];
+            string aliments;
+            mesChiens[0] = new Chien("Marcus", 4, "Korgui");
+            mesChiens[1] = new Chien("Paul", 5, "Bouldog");
+            mesChiens[2] = new Chien("Guillaume", 1, "saucisse");
+
+            for (int i = 0; i < mesChiens.Length; i++)
+            {
+                Console.WriteLine(mesChiens[i].Affichercaracteristique());
+                Console.WriteLine(mesChiens[i].Aboyer());
+                mesChiens[i].Aliment(out aliments);
+                Console.WriteLine(mesChiens[i].Manger(aliments));
+                mesChiens[i].veillir();
+                Console.WriteLine(mesChiens[i].Affichercaracteristique());
+            }
+        }
+
+    }
+}
