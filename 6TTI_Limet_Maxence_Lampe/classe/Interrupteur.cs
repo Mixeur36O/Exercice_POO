64aa5eb0cd291e5673766b7f7a70f835cf066f0f
Maxence <200040@site.asty-moulin.be>
2025-12-10 18:23:54 +0100

    10/12/2025

---
+++ 6TTI_Limet_Maxence_Lampe/classe/Interrupteur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6TTI_Limet_Maxence_Lampe.classe
+{
+    internal class Interrupteur
+    {
+        //Attributs
+        private bool _allumage;
+
+        //Props
+        public bool Allumage
+        {
+            get { return _allumage; }
+            set { _allumage = value; }
+        }
+
+
+        //Constru
+        public Interrupteur(bool allumage)
+        {
+            _allumage = allumage;
+        }
+
+        //Méthodes
+        public string AllumageL()
+        {
+            if (_allumage == true)
+            {
+                string info = "Votre lampe est allumé";
+                return info;
+            }
+            else
+            {
+                string info = "Votre lampe est étteinte";
+                return info;
+            }
+        }
+
+    }
+}
---
+++ 6TTI_Limet_Maxence_Lampe/classe/Lampe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6TTI_Limet_Maxence_Lampe.classe
+{
+    internal class Lampe
+    {
+        //Attributs
+        private string _couleur;
+        private string _type;
+
+        //Props
+        public string Couleur
+        {
+            get { return _couleur; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+        }
+
+        //Constru
+        public Lampe(string couleur, string type)
+        {
+            _couleur = couleur;
+            _type = type;
+        }
+
+        //Méthodes
+        public string CaractéristiqueL()
+        {
+            string infos = $"Lampe de couleur : {_couleur}, elle est sur {_type}";
+            return infos;
+        }
+
+    }
+}
