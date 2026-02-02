44ff6a6aeda6b2837442ce5d273346315fe277b9
Limet Maxence <200040@CCM-Asty-Moulin.local>
2026-02-02 10:27:09 +0100

    02/02/2026 10:16

---
+++ 6TTI_Limet_Maxence_Bibli2 - Copie/Program.cs
@@ -0,0 +1,101 @@
+﻿using _6TTI_Limet_Maxence_Bibli.classe;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _6TTI_Limet_Maxence_Bibli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string recommencer = "";
+            Bibliotheque biblio = new Bibliotheque();
+            Livre livre = new Livre("","",0);
+
+            string choixC = "";
+
+
+            do
+            {
+                Console.WriteLine("Bonjour et bienvenu dans notre bibliothèque. \n Que voulez vous faire ?");
+                Console.WriteLine("C : Créer un Livre \n" + 
+                                  "D : Dégrader un livre \n" + 
+                                  "A : Abonner vous \n" +
+                                  "I : Inventaire \n" +
+                                  "E : Emprunter un Livre" +
+                                  "S : Supprimer les livre trop abîmer");
+                ConsoleKeyInfo info = Console.ReadKey(true);
+                if (info.Key == ConsoleKey.C)
+                {
+                    string titre = "";
+                    string auteur = "";
+                    int etat = 5;
+                    Console.WriteLine("Quel titre aimeriez-vous donner à votre livre");
+                    titre = Console.ReadLine();
+                    Console.WriteLine("Veuillez donner votre nom pour votre livre");
+                    auteur = Console.ReadLine();
+                    livre = new Livre(titre, auteur, etat);
+                    biblio.Ajoute(livre);
+                }
+
+                if(info.Key == ConsoleKey.D)
+                {
+                    livre.Degrade();
+                    Console.WriteLine("Dégradation du Livre réussis");
+                }
+
+                if(info.Key == ConsoleKey.A)
+                {
+                    string nom = "";
+                    string prenom = "";
+                    string email = "";
+                    Console.WriteLine("Quel est votre nom ?");
+                    nom = Console.ReadLine();
+                    Console.WriteLine("Quel est votre prénom ?");
+                    prenom = Console.ReadLine();
+                    Console.WriteLine("Entrer votre adresse mail");
+                    email = Console.ReadLine();
+                    biblio.CreeAbonne(nom, prenom, email);
+                }
+                
+                if (info.Key == ConsoleKey.I)
+                {
+                    Console.WriteLine(biblio.inventaire());
+                }
+
+                if (info.Key == ConsoleKey.E)
+                {
+                    string titre = "";
+                    Console.WriteLine("Choisisser un livre à emprunter")
+                    titre = Console.ReadLine();
+                    if (TrouveLivre(titre, biblio, out livre))
+                    biblio.AjouteEmpruntLivre(livre, );
+                }
+
+                if (info.Key == ConsoleKey.S)
+                {
+
+                }
+
+                Console.WriteLine("Voulez-vous réemprunter un livre");
+                recommencer = Console.ReadLine();
+            } while (recommencer != "non");
+            
+        }
+        static bool TrouveLivre(string titre, List<Livre> biblio, out Livre livre)
+        {
+            bool trouve = false;
+            livre = null;
+            foreach (Livre item in biblio)
+            {
+                if(item.Titre == titre)
+                {
+                    livre = item;
+                    trouve = true;
+                }
+            }
+            return trouve;
+        }
+    }
+}
---
+++ 6TTI_Limet_Maxence_Bibli2 - Copie/classe/Abonne.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_Limet_Maxence_Bibli.classe
+{
+    internal class Abonne
+    {
+        //Attributs
+        private string _nom;
+        private string _prenom;
+        private string _email;
+
+        //Props
+        public string Nom
+        {
+            get { return _nom; }
+        }
+        public string Prenom
+        {
+            get { return _prenom; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        //Construct
+        public Abonne(string nom, string prenom, string email)
+        {
+            _nom = nom;
+            _prenom = prenom;
+            _email = email;
+        }
+
+        //Méthodes
+
+        public string infos()
+        {
+            string info = $"L'abboner s'appelle{_nom} {_prenom}. Son adresse mail est {_email}";
+            return info;
+        }
+    }
+}
